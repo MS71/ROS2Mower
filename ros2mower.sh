79d35b8c9e988f3445780d07637fe404123d95a1
MS71 <MS71>
2019-08-13 19:02:58 +0000

    -

--- ros2mower.sh
+++ ros2mower.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-echo "ROS2Mower ..."
+cd $(dirname $(readlink -f "$0"))
+echo "ROS2Mower $1 ..."
+if [ $1 = "start" ] ; then
+  git pull . 
+  . /opt/ros/dashing/local_setup.sh
+  ros2 run demo_nodes_cpp talker
+fi
 echo "ROS2Mower ... done"

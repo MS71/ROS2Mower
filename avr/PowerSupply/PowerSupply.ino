dc65a270fa83c3ff64f1397da1fd9358d77af7c1
MS <Scholz.Maik@t-online.de>
2019-08-14 08:46:55 +0200

    added

---
+++ avr/PowerSupply/PowerSupply.ino
@@ -0,0 +1,17 @@
+#include "pm.h"
+#include "i2c.h"
+//
+void setup()
+{
+  pm_init();
+  i2c_init();
+}
+
+//
+void loop()
+{
+  i2c_loop();
+  pm_loop();
+}
+
+// EOF
---
+++ avr/PowerSupply/pm.h
@@ -0,0 +1,4 @@
+void i2c_init();
+void i2c_loop();
+
+

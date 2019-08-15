bfa4a578e3d680097ecca4280d9eba21a3058d62
MS71 <MS71>
2019-08-15 15:34:12 +0000

    added

---
+++ ros2_ws/b.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+colcon build --symlink-install
---
+++ ros2_ws/r.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+. /opt/ros/dashing/local_setup.sh
+. ./install/local_setup.sh
+ros2 launch r2m_robot_bringup r2m_robot_bringup.launch.py

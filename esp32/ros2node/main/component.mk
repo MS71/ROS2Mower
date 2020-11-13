f7c1c5f16cb81632906af25f7798c24f306004c3
MS71 <MS71>
2020-11-13 19:59:26 +0100

    current sw

--- esp32/ros2node/main/component.mk
+++ esp32/ros2node/main/component.mk
@@ -1,5 +1,14 @@
 #
-# "main" pseudo-component makefile.
+# Main Makefile. This is basically the same as a component makefile.
 #
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
+# This Makefile should, at the very least, just include $(SDK_PATH)/make/component.mk. By default, 
+# this will take the sources in the src/ directory, compile them and link them into 
+# lib(subdirectory_name).a in the build directory. This behaviour is entirely configurable,
+# please read the SDK documents if you need to do this.
+#
+ULP_APP_NAME ?= ulp-$(COMPONENT_NAME)
+ULP_S_SOURCES = $(addprefix $(COMPONENT_PATH)/ulp/, \
+	blink.S \
+	)
+#ULP_EXP_DEP_OBJECTS := main.o ulp-util.o
+include $(IDF_PATH)/components/ulp/component_ulp_common.mk
